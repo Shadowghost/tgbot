0cebe81dc29a2509ad6191ce87639c9bbb05607e
YasiR Siddiqui <38703416+Yasir-siddiqui@users.noreply.github.com>
2018-08-16 19:08:38 +0530

    update sample config

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -24,6 +24,7 @@ class Config(object):
 
     # OPTIONAL
     SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
+	SECRET_SUDO_USERS = [] # List of id's (not usernames) for users which can gban/ungban gmute/ungmute stats/chatlist gbanlist/gmutelist but are no globa admins.
     SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = None  # EG, paypal
