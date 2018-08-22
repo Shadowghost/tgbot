ece4dea61be1682b2ef6f36c599dc013377950e8
Shadowghost <Ghost_of_Stone@web.de>
2018-08-22 19:29:54 +0200

    Fix lyrics module error reply and help text

--- tg_bot/modules/lyrics.py
+++ tg_bot/modules/lyrics.py
@@ -1,12 +1,13 @@
 from PyLyrics import *
+from requests import get
+from typing import Optional, List
+
 from telegram import Update, Bot, ParseMode
 from telegram.ext import run_async
 from typing import Optional, List
 
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot import dispatcher
-
-from requests import get
 
 LYRICSINFO = "\n\n[Full Lyrics](http://lyrics.wikia.com/wiki/%s:%s)"
 
@@ -17,8 +18,8 @@ def lyrics(bot: Bot, update: Update, args: List[str]):
 
     reply_text = msg.reply_to_message.reply_text if msg.reply_to_message else msg.reply_text
 
-    if len(args) == 0 and msg.reply_to_message.text == None :
-        msg.reply_text("Invalid syntax - correct syntax:  `/lyrics Artist - Song` or quote", parse_mode=ParseMode.MARKDOWN, quote=True, failed=True)
+    if len(args) == 0 and msg.reply_to_message == None :
+        reply_text("Invalid syntax - correct syntax:  `/lyrics <artist> - <song>` or quote", parse_mode=ParseMode.MARKDOWN, quote=True, failed=True)
         return
 
     elif len(args) >= 1:
@@ -40,17 +41,18 @@ def lyrics(bot: Bot, update: Update, args: List[str]):
             lyrics = "\n".join(PyLyrics.getLyrics(
                 song[0], song[1]).split("\n")[:10])
         except ValueError as e:
-            return reply_text("Song %s not found :(" % song[1], quote=True, failed=True)
+            reply_text("Song _%s_ not found :(" % song[1], parse_mode=ParseMode.MARKDOWN, quote=True, failed=True)
+            return
         else:
             lyricstext = LYRICSINFO % (song[0].replace(
                 " ", "_"), song[1].replace(" ", "_"))
             return reply_text(lyrics + lyricstext, parse_mode=ParseMode.MARKDOWN, quote=True, disable_web_page_preview=True)
     else:
-        return reply_text("Invalid syntax - correct syntax:  `/lyrics Artist - Song`", parse_mode=ParseMode.MARKDOWN, quote=True, failed=True)
+        reply_text("Invalid syntax - correct syntax:  `/lyrics <artist> - <song>` or quote", parse_mode=ParseMode.MARKDOWN, quote=True, failed=True)
 
 
 __help__ = """
- - /lyrics <artist - <song>: Find your favourite songs' lyrics
+ - /lyrics <artist> - <song>: Find your favourite songs' lyrics
 """
 
 
